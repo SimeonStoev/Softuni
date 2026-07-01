cd49f27d56412b4086b43367ae3b46a33008e10d
SimeonStoev <simeon.stoev.95@gmail.com>
2026-07-01 22:20:35 +0300

    Exer task 7

---
+++ Python_OOP/Classes_And_Objects/project3/band.py
@@ -0,0 +1,29 @@
+from project3.album import Album
+
+
+class Band:
+    def __init__(self, name):
+        self.name = name
+        self.albums = []
+
+    def add_album(self, album: Album):
+        if album not in self.albums:
+            self.albums.append(album)
+            return f"Band {self.name} has added their newest album {album.name}."
+        return f"Band {self.name} already has {album.name} in their library."
+
+    def remove_album(self, album_name: str):
+        for album in self.albums:
+            if album.name == album_name and not album.published:
+                self.albums.remove(album)
+                return f"Album {album_name} has been removed."
+            elif album.published:
+                return "Album has been published. It cannot be removed."
+
+        return f"Album {album_name} is not found."
+
+    def details(self):
+        result = f"Band {self.name}\n"
+        for album in self.albums:
+            result += f"{album.details()}\n"
+        return result
